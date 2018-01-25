1324d4aba845513a1be9e2d93c385b1f579dc6e8
chinahnjlk <chinahnjlk@gmail.com>
2018-01-25 23:00:12 +0800

    Events

--- src/DotNetFW/Program.cs
+++ src/DotNetFW/Program.cs
@@ -12,32 +12,75 @@ namespace DotNetFW
     {
         static void Main(string[] args)
         {
-            DriveInfo[] allDrives = DriveInfo.GetDrives();
+            //  Create a FileSystemWatcher to monitor all files on drive C.
+            FileSystemWatcher fsw = new FileSystemWatcher("D:\\");
 
-            foreach (DriveInfo d in allDrives)
-            {
-                Console.WriteLine("Drive {0}", d.Name);
-                Console.WriteLine("  Drive type: {0}", d.DriveType);
-                if (d.IsReady)
-                {
-                    Console.WriteLine("  Volume label: {0}", d.VolumeLabel);
-                    Console.WriteLine("  File system: {0}", d.DriveFormat);
-                    Console.WriteLine(
-                        "  Available space to current user:{0, 15} bytes",
-                        d.AvailableFreeSpace);
+            //  Watch for changes in LastAccess and LastWrite times, and
+            //  the renaming of files or directories. 
+            fsw.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite
+                               | NotifyFilters.FileName | NotifyFilters.DirectoryName;
 
-                    Console.WriteLine(
-                        "  Total available space:          {0, 15} bytes",
-                        d.TotalFreeSpace);
+            //  Register a handler that gets called when a 
+            //  file is created, changed, or deleted.
+            fsw.Changed += OnChanged;
 
-                    Console.WriteLine(
-                        "  Total size of drive:            {0, 15} bytes ",
-                        d.TotalSize);
-                }
-            }
+            fsw.Created += OnChanged;
+
+            fsw.Deleted += OnChanged;
+
+            //  Register a handler that gets called when a file is renamed.
+            fsw.Renamed += OnRenamed;
+
+            //  Register a handler that gets called if the 
+            //  FileSystemWatcher needs to report an error.
+            fsw.Error += OnError;
+
+            //  Begin watching.
+            fsw.EnableRaisingEvents = true;
+
+            Console.WriteLine("Press \'Enter\' to quit the sample.");
+            Console.ReadLine();
+
+
 
         }
+
+
+        private static void OnChanged(object source, FileSystemEventArgs e)
+        {
+            //  Show that a file has been created, changed, or deleted.
+            WatcherChangeTypes wct = e.ChangeType;
+            Console.WriteLine("File {0} {1}", e.FullPath, wct.ToString());
+        }
+
+
+        private static void OnRenamed(object source, RenamedEventArgs e)
+        {
+            //  Show that a file has been renamed.
+            WatcherChangeTypes wct = e.ChangeType;
+            Console.WriteLine("File {0} {2} to {1}", e.OldFullPath, e.FullPath, wct.ToString());
+        }
+
+        //  This method is called when the FileSystemWatcher detects an error.
+        private static void OnError(object source, ErrorEventArgs e)
+        {
+            //  Show that an error has been detected.
+            Console.WriteLine("The FileSystemWatcher has detected an error");
+            //  Give more information if the error is due to an internal buffer overflow.
+            if (e.GetException().GetType() == typeof(InternalBufferOverflowException))
+            {
+                //  This can happen if Windows is reporting many file system events quickly 
+                //  and internal buffer of the  FileSystemWatcher is not large enough to handle this
+                //  rate of events. The InternalBufferOverflowException error informs the application
+                //  that some of the file system events are being lost.
+                Console.WriteLine(("The file system watcher experienced an internal buffer overflow: " + e.GetException().Message));
+            }
+        }
     }
+
+
+
+   
 
     public class Test
     {
@@ -102,7 +145,7 @@ namespace DotNetFW
         //byte[] bb = Encoding.ASCII.GetBytes(cc);
         #endregion
 
-        #region BinaryWriter 
+        #region BinaryWriter 类
 
         //string path = "D:/10.txt";
         //if (!File.Exists(path))
@@ -121,6 +164,33 @@ namespace DotNetFW
         ////bw.Write();
         //Console.ReadKey();
 
+        #endregion
+
+        #region DriveInfo 类
+
+        //DriveInfo[] allDrives = DriveInfo.GetDrives();
+
+        //foreach (DriveInfo d in allDrives)
+        //{
+        //    Console.WriteLine("Drive {0}", d.Name);
+        //    Console.WriteLine("  Drive type: {0}", d.DriveType);
+        //    if (d.IsReady)
+        //    {
+        //        Console.WriteLine("  Volume label: {0}", d.VolumeLabel);
+        //        Console.WriteLine("  File system: {0}", d.DriveFormat);
+        //        Console.WriteLine(
+        //            "  Available space to current user:{0, 15} bytes",
+        //            d.AvailableFreeSpace);
+
+        //        Console.WriteLine(
+        //            "  Total available space:          {0, 15} bytes",
+        //            d.TotalFreeSpace);
+
+        //        Console.WriteLine(
+        //            "  Total size of drive:            {0, 15} bytes ",
+        //            d.TotalSize);
+        //    }
+        //}
         #endregion
     }
 }
